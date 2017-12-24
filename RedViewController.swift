b6ff54799a111ba0ff8065d2cbb2129bd2a44f66
Andres G <andres_gu@icloud.com>
2017-12-23 17:40:28 -0700

    RainbowTabs

---
+++ RedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RedViewController.swift
+//  RainbowTabs
+//
+//  Created by Andres Gutierrez on 12/23/17.
+//  Copyright © 2017 Andres Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = nil
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
